38c592ac263fa54738ff27d03170807c935f2b3a
abrahamuchos <agonzalez@wayuinc.com>
2020-05-11 23:30:20 -0400

    Working

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,11 +18,23 @@ use Illuminate\Support\Str;
 */
 
 $factory->define(User::class, function (Faker $faker) {
-    return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
-    ];
+  $gender = (random_int(0,1) == 1) ? 'male' : 'female';
+  return [
+    'email' => $faker->unique()->safeEmail,
+    'is_admin' => $faker->boolean(40),
+    'name' => $faker->firstName($gender),
+    'last_name' => $faker->lastName,
+    'avatar' => ($gender == 'male') ? 'Uploads/Profiles/avatar-male.svg' : 'Uploads/Profiles/avatar-female.svg',
+    'email_verified_at' => now(),
+    'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+    'remember_token' => Str::random(10),
+    'document'=> (random_int(0,1) == 1) ? random_int(1303001,25258905) : null,
+    'country' => $country = (random_int(0,1) == 1) ? $faker->country : null,
+    'state' => ($country) ? $faker->state : null,
+    'city' => ($country) ? $faker->city : null,
+    'address_line1' => ($country) ? $faker->streetName : null,
+    'address_line2' => ($country) ? $faker->streetAddress : null,
+    'phone_number' => ($country) ? $faker->e164PhoneNumber :null,
+    'is_subscriber' => $faker->boolean(30)
+  ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,13 +4,32 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call(UserSeeder::class);
+  /**
+   * Seed the application's database.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    $this->call([
+      UserSeeder::class
+    ]);
+
+//    To delete table
+//    $this->truncateTable([
+//      'users',
+//    ]);
+  }
+
+  /**
+   * @param array $tables
+   */
+  private function truncateTable(array $tables): void
+  {
+//    DB::statement('SET FOREING_KEY_CHECKS = 0;');
+    foreach ($tables as $table) {
+      DB::table($table)->truncate();
     }
+//    DB::statement('SET FOREING_KEY_CHECKS = 1;');
+  }
 }
